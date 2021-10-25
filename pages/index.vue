828dab455bd318079d53429d9e7e84391003b9e1
Vishnu Prasath R <65113867+VishnuPrasath-R@users.noreply.github.com>
2021-10-25 19:34:54 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,17 @@
-<template>
-  <div id="home-page" class="page-wrapper home-page">
-    <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
-      <button
-        v-if="$siteConfig.newsletter.on"
-        class="button is-primary"
-        @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
-      >
-        Subscribe To Newsletter
-      </button>
-    </site-hero>
-    <main-section theme="one-column">
-      <template v-slot:default>
-        <!-- All Posts -->
-        <posts-grid />
-      </template>
-      <template v-slot:sidebar>
-        Nothing here
-      </template>
-    </main-section>
-    <news-letter-form-modal />
-  </div>
-</template>
-
-<script>
-import { mapState } from 'vuex'
-import { setPageData } from '../helper'
-import NewsLetterFormModal from '~/components/NewsLetterFormModal'
-
-export default {
-  name: 'HomePage',
-  head() {
-    return {
-      title: `Home | ${this.$siteConfig.siteName}`
-    }
-  },
-  components: {
-    NewsLetterFormModal
-  },
-  computed: {
-    ...mapState(['title', 'subtitle', 'featureImage'])
-  },
-  fetch({ store, params }) {
-    setPageData(store, { slug: 'home' })
-  }
-}
-</script>
-
+<!DOCTYPE html>
+<html>
+<head>
 <style>
-.home-page .under-subtitle {
-  border-top: none;
+img {
+  width: 100%;
 }
 </style>
+</head>
+<body>
+
+<img src="html5.gif" alt="HTML5 Icon" width="128" height="128">
+
+<img src="html5.gif" alt="HTML5 Icon" style="width:128px;height:128px;">
+
+</body>
+</html>
